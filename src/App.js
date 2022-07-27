e0b9fed3e1c6a208cb0773c823d4db47226133d2
bennywu2019 <benny.wu.new@gmail.com>
2022-07-27 17:45:44 -0400

    remove bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/pages/Home";
-import "bootstrap/dist/css/bootstrap.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
