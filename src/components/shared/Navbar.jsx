d4d47567b9cc42ba224b595d4e7213a9a18af832
bennywu2019 <benny.wu.new@gmail.com>
2023-05-13 20:06:28 -0400

    created structure

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return <div className="nav-container">
+        <h2>Ugly nav bar</h2>
+        <Link></Link>
+    </div>
+}
+export default Navbar;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Homepage.css";
+
+const Homepage = () => {
+    return <div className="home-page">
+        <h1 className="home-title">We touch fish</h1>
+    </div>
+}
+export default Homepage;
