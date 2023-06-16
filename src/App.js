e0ceb5b8070b5fe9c1d064aecbbc525837f7055f
bennywu2019 <benny.wu.new@gmail.com>
2023-06-15 22:38:14 -0400

    completed styling for chat page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,43 @@ import LoginPage from "./pages/HouseChat/LoginPage";
 import RegistrationPage from "./pages/HouseChat/RegistrationPage";
 import ChatPage from "./pages/HouseChat/ChatPage";
 
+const DUMMY_USER = {
+  name: "Doraemon",
+  totalPosts: 1,
+  totalDays: 3,
+  avatarPath: "./assets/Doraemon.png",
+  posts: [
+    {
+      user: "Benny_Cooldkid",
+      avatarPath: "./assets/Doraemon.png",
+      text: "I really hope I can pull thsi through. The design looks great but I don't know what will happen when I program it out lol",
+      date: "2023-06-10",
+    },
+    {
+      user: "KevinWang",
+      avatarPath: "./assets/Doreamon.png",
+      text: "I love Benny",
+      date: "2023-06-09",
+    },
+    {
+      user: "Pournasrola",
+      avatarPath: "./assets/Doreamon.png",
+      text: "Oyaya is an attitude toward life.\n \
+      It is resistance to the era of involution. \n \
+      It symbolizes the resistance against captilism \n \
+      Only by mastering the spirit of oyaya, \n \
+      can you truly enjoy your life.",
+      date: "2023-06-09",
+    },
+    {
+      user: "Jilao",
+      avatarPath: "./assets/Doraemon.png",
+      text: "ababab",
+      date: "2023-06-08",
+    },
+  ],
+};
+
 // this is the recommended router
 // https://reactrouter.com/en/main/routers/create-browser-router
 const router = createBrowserRouter([
@@ -19,7 +56,7 @@ const router = createBrowserRouter([
   { path: "/food-ordering", element: <FoodPage /> },
   { path: "/chat/login", element: <LoginPage /> },
   { path: "/chat/registration", element: <RegistrationPage /> },
-  { path: "/chat", element: <ChatPage /> },
+  { path: "/chat", element: <ChatPage user={DUMMY_USER} /> }, // Dummy Data
 ]);
 
 function App() {
--- src/pages/HouseChat/ChatPage.jsx
+++ src/pages/HouseChat/ChatPage.jsx
@@ -1,11 +1,61 @@
 import "./ChatPage.css";
+import Avatar from "../../assets/Doraemon.png";
+import Button from "../../components/shared/Button";
 
-const ChatPage = () => {
+const ChatPage = ({ user }) => {
+  const userInfoText = (
+    <div className="user-info-text">
+      <div className="user-avatar">
+        <img src={Avatar} alt="Profile picture" />
+      </div>
+      <div className="user-name">
+        <span>Welcome back,</span>
+        <h2>{user.name}</h2>
+      </div>
+      <div className="user-stats">
+        <h4>Statistics</h4>
+        <span>Total Posts: {user.totalPosts} </span>
+        <span>Registered For: {user.totalDays} Days </span>
+      </div>
+      <div className="user-logout">
+        <Button
+          height="40px"
+          width="128px"
+          color="delft-blue"
+          border="black solid 0.5px"
+          onSubmit={() => {}}
+        >
+          Log out
+        </Button>
+      </div>
+    </div>
+  );
+
+  const chatThread = user.posts.map((post) => (
+    <div className="chat-post" id={post.user}>
+      <header>
+        <img src={Avatar} alt="profile picture" />
+        <span className="post-user">{post.user}</span>
+        <span className="post-date">{post.date}</span>
+      </header>
+      <body>{post.text}</body>
+    </div>
+  ));
+
+  console.log(chatThread);
+
+  console.log(chatThread);
   return (
     <div className="chat-container">
       <div className="chat-boxes">
-        <div className="user-info"></div>
-        <div className="chat-thread"></div>
+        <div className="user-info">
+          <div className="user-background" />
+          {userInfoText}
+        </div>
+        <div className="chat-thread">
+          <div className="chat-background" />
+          <div className="chat-thread-text">{chatThread}</div>
+        </div>
       </div>
     </div>
   );
